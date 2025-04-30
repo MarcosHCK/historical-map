fb3403690107d8c3103c57f6d39ef19bdc31094a
MarcosHCK <marcoshck16@gmail.com>
2025-04-30 09:17:48 -0400

    Better controls

--- components/Map.tsx
+++ components/Map.tsx
@@ -14,12 +14,12 @@
  * You should have received a copy of the GNU General Public License
  * along with Historical-Map. If not, see <http://www.gnu.org/licenses/>.
  */
-import { Button, Center, Group, Overlay, Popover, Slider, Stack, Transition } from '@mantine/core'
+import { Button, Center, Group, Overlay, Popover, Skeleton, Slider, Stack, Transition } from '@mantine/core'
 import { PiFastForwardFill, PiPauseFill, PiPlayFill, PiStopFill } from 'react-icons/pi'
 import { type SVGPathProperties, useMapWalkPath } from '../hooks/useMapWalkPath'
 import { useAnimator } from '../hooks/useAnimator'
 import { useCallback, useEffect, useMemo, useState } from 'react'
-import { useDebouncedValue, useDisclosure } from '@mantine/hooks'
+import { useDebouncedValue, useDisclosure, useHover } from '@mantine/hooks'
 import { useMapDescription } from '../hooks/useMapDescription'
 import { useTrigger } from '../hooks/useTrigger'
 import css from './Map.module.css'
@@ -37,12 +37,12 @@ interface MapCanvasProps
 
 const MapCanvas = ({ cursor = '/cursor.svg', texture, path, scale }: MapCanvasProps) =>
 {
-  const [ firstFire, setFirstFire ] = useState (true)
   const [ playing, { close, toggle } ] = useDisclosure (false)
   const [ debouncedPlaying ] = useDebouncedValue (playing, controlTimeout)
   const [ showControl, fireShowControl ] = useTrigger (false, true, controlTimeout)
   const [ velocity, setVelocity ] = useState (100)
   const [ ref, { pause, play, present, ready: _ready, reset } ] = useAnimator (cursor, path, texture, scale * velocity / 100)
+  const { ref: overlayRef, hovered: hoverBar } = useHover ()
   const ready = useMemo (() => !! cursor && !! path && !! texture && _ready, [cursor, path, texture, _ready])
 
   useEffect (() => { switch (playing)
@@ -58,7 +58,6 @@ const MapCanvas = ({ cursor = '/cursor.svg', texture, path, scale }: MapCanvasPr
   const resetAnimation = useCallback (() => { if (ready)
     {
       close ()
-      setFirstFire (false)
       pause ()
       reset ()
     }}, [close, ready, reset, pause])
@@ -67,50 +66,76 @@ const MapCanvas = ({ cursor = '/cursor.svg', texture, path, scale }: MapCanvasPr
     {
       toggle ()
       fireShowControl ()
-      setFirstFire (false)
     }}, [fireShowControl, ready, toggle])
 
   return <Center> <Stack className={css.canvasContainer}>
 
     <canvas ref={ref} />
 
-    <Overlay backgroundOpacity={0} zIndex={2}>
+    <Overlay backgroundOpacity={0} hidden={ready} zIndex={1}>
+
+      <Stack gap={6}>
+        <Group justify='space-between'>
+
+          <Skeleton height={50} circle mb='lg' />
+          <Stack>
+            <Skeleton height={50} />
+            <Skeleton height={50} width='70%' />
+          </Stack>
+        </Group>
+
+        <Skeleton height={8} radius='xl' />
+        <Skeleton height={8} radius='xl' />
+        <Skeleton height={8} radius='xl' width='70%' />
+        <Skeleton height={8} radius='xl' />
+        <Skeleton height={8} radius='xl' />
+        <Skeleton height={8} radius='xl' width='95%' />
+        <Skeleton height={8} radius='xl' />
+        <Skeleton height={8} radius='xl' />
+        <Skeleton height={8} radius='xl' width='60%' />
+      </Stack>
+    </Overlay>
+
+    <Overlay backgroundOpacity={0} ref={overlayRef} zIndex={2}>
+
+      <Transition mounted={hoverBar}>
+
+        { style => 
+        <Group className={css.canvasGroup} gap={7} justify='start' style={style}>
+
+          <Button color='white' onClick={resetAnimation} variant='transparent'>
+            <PiStopFill />
+          </Button>
+
+          <Button color='white' onClick={toggleAnimation} variant='transparent'>
+            { playing ? <PiPauseFill /> : <PiPlayFill /> }
+          </Button>
+
+          <Popover position='top-start' withinPortal={false}>
+
+            <Popover.Target>
+
+              <Button color='white' variant='transparent'> <PiFastForwardFill /> </Button>
+            </Popover.Target>
+
+            <Popover.Dropdown w={300}>
+
+              <Slider color='blue' marks={[ 25, 50, 75, 100, 125, 150, 175, 200 ].map (e => ({ value: e }))}
+                min={1} max={200} onChange={setVelocity} restrictToMarks value={velocity} />
+            </Popover.Dropdown>
+          </Popover>
+        </Group> }
+      </Transition>
 
       <Center className={css.canvasCenter} onClick={toggleAnimation}>
 
-        <Transition duration={transitionTime} mounted={firstFire || showControl} transition='fade'>
+        <Transition duration={transitionTime} mounted={! playing || showControl} transition='fade'>
           { style => <Button className={css.controlButton} color='white' radius='xl' size='xl' style={style} variant='transparent'
               loading={! ready} onClick={toggleAnimation}>
             { debouncedPlaying ? <PiPauseFill /> : <PiPlayFill /> }
           </Button> }
         </Transition>
       </Center>
-
-      { firstFire ||
-      <Group className={css.canvasGroup} gap={7} justify='end'>
-
-        <Button color='white' onClick={resetAnimation} variant='transparent'>
-          <PiStopFill />
-        </Button>
-
-        <Button color='white' onClick={toggleAnimation} variant='transparent'>
-          { playing ? <PiPauseFill /> : <PiPlayFill /> }
-        </Button>
-
-        <Popover position='bottom-end'>
-
-          <Popover.Target>
-
-            <Button color='white' variant='transparent'> <PiFastForwardFill /> </Button>
-          </Popover.Target>
-
-          <Popover.Dropdown w={300}>
-
-            <Slider color='blue' marks={[ 25, 50, 75, 100, 125, 150, 175, 200 ].map (e => ({ value: e }))}
-              min={1} max={200} onChange={setVelocity} restrictToMarks value={velocity} />
-          </Popover.Dropdown>
-        </Popover>
-      </Group> }
     </Overlay>
   </Stack> </Center>
 }
